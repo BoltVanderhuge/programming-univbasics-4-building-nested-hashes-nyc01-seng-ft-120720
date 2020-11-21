c311f8fcd643e3e03694a037c9585fd8efd96af1
John Wisneski <john.j.wisneski@gmail.com>
2020-11-21 23:58:01 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -23,7 +23,7 @@ base = {
        :two_pieces_owned => 50,
        :three_pieces_owned => 100,
        :four_pieces_owned => 200
-     }
+     },
      :names => {
        :reading_railroad => {},
        :pennsylvania_railroad => {},
